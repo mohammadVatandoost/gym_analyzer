2b27bd93b113f1efa2c26d1eb87f45247a4efbea
mohammadVatandoost <mohamadvatandoost512@gmail.com>
2024-01-14 09:54:00 +0100

    save

--- pkg/pose/lib_openpose.py
+++ pkg/pose/lib_openpose.py
@@ -1,45 +1,16 @@
-import os
-import re
-import sys
-# sys.path.append('.')
-import cv2
-import math
-import time
-import scipy
-import argparse
-import matplotlib
+import logging
 import numpy as np
-import pylab as plt
 import torch
-import torch.nn as nn
-import torch.nn.functional as F
-from torch.autograd import Variable
-from collections import OrderedDict
-from scipy.ndimage import generate_binary_structure
-from scipy.ndimage import gaussian_filter, maximum_filter
-
-from lib.network.rtpose_vgg import get_model
-from lib.network import im_transform
-from evaluate.coco_eval import get_outputs, handle_paf_and_heat
-from lib.utils.common import Human, BodyPart, CocoPart, CocoColors, CocoPairsRender, draw_humans
-from lib.utils.paf_to_pose import paf_to_pose_cpp
-from lib.config import cfg, update_config
+import cv2
+from pkg.pose import im_transform
+from pkg.pose.draw import Human, BodyPart, CocoPart, CocoColors, CocoPairsRender, draw_humans
+from pkg.pose.paf_to_pose import paf_to_pose_cpp
+from pkg.pose.config import _C as cfg
 from pkg.video_reader.video_reader import VideoReader
-
-
-# parser = argparse.ArgumentParser()
-# parser.add_argument('--cfg', help='experiment configure file name',
-#                     default='./experiments/vgg19_368x368_sgd.yaml', type=str)
-# parser.add_argument('--weight', type=str,
-#                     default='pose_model.pth')
-# parser.add_argument('opts',
-#                     help="Modify config options using the command-line",
-#                     default=None,
-#                     nargs=argparse.REMAINDER)
-# args = parser.parse_args()
-#
-# # update config file
-# update_config(cfg, args)
+from pkg.pose.rtpose_vgg import get_model
+from pkg.pose.preprocessing import (inception_preprocess,
+                                              rtpose_preprocess,
+                                              ssd_preprocess, vgg_preprocess)
 
 class OpenPose():
     def __init__(self, video_reader: VideoReader) -> None:
@@ -54,15 +25,49 @@ class OpenPose():
     def estimate(self):
         oriImg = self.video_reader.read_frame()
 
-        # shape_dst = np.min(oriImg.shape[0:2])
-
         with torch.no_grad():
-            paf, heatmap, im_scale = get_outputs(oriImg, self.model, 'rtpose')
+            paf, heatmap, im_scale = self.get_outputs(oriImg, self.model, 'rtpose')
 
-        print(im_scale)
+        # print(im_scale)
         humans = paf_to_pose_cpp(heatmap, paf, cfg)
-
+        logging.info(f"humans = {humans}")
         out = draw_humans(oriImg, humans)
         return out
         # cv2.imwrite('result.png', out)
+
+    def get_outputs(self, img, model, preprocess):
+        """Computes the averaged heatmap and paf for the given image
+        :param multiplier:
+        :param origImg: numpy array, the image being processed
+        :param model: pytorch model
+        :returns: numpy arrays, the averaged paf and heatmap
+        """
+        inp_size = cfg.DATASET.IMAGE_SIZE
+
+        # padding
+        im_croped, im_scale, real_shape = im_transform.crop_with_factor(
+            img, inp_size, factor=cfg.MODEL.DOWNSAMPLE, is_ceil=True)
+
+        if preprocess == 'rtpose':
+            im_data = rtpose_preprocess(im_croped)
+
+        elif preprocess == 'vgg':
+            im_data = vgg_preprocess(im_croped)
+
+        elif preprocess == 'inception':
+            im_data = inception_preprocess(im_croped)
+
+        elif preprocess == 'ssd':
+            im_data = ssd_preprocess(im_croped)
+
+        batch_images = np.expand_dims(im_data, 0)
+
+        # several scales as a batch
+        batch_var = torch.from_numpy(batch_images).cuda().float()
+        predicted_outputs, _ = model(batch_var)
+        output1, output2 = predicted_outputs[-2], predicted_outputs[-1]
+        heatmap = output2.cpu().data.numpy().transpose(0, 2, 3, 1)[0]
+        paf = output1.cpu().data.numpy().transpose(0, 2, 3, 1)[0]
+
+        return paf, heatmap, im_scale
 
